1d8ab5e0eb93187e99b8c419fa634af358770857
Jason Te <te.jason.d@gmail.com>
2026-04-25 20:31:25 +0800

    💚 fix missing assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/docs/',
   plugins: [
     react(),
     TanStackRouterVite(),
